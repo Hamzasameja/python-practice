f864106b70bfc6f0b70f80e2999f68a0098a599d
Hamzasameja <hamzayousufsameja@gmail.com>
2026-08-08 14:47:58 -0700

    Day 2: varaibles, strings, input, type conversion

---
+++ python-practice/day2.py
@@ -0,0 +1,27 @@
+# # # # # print("Hello, my name is ----")
+# # # # name = ("Hamza")
+# # # # print(name)
+
+# # # # name = "Hamza"
+# # # # print("Hello, my name is " + name)
+
+# # # # num1 = 10
+# # # # num2 = 5
+# # # # sum = num1 + num2
+# # # # print("the sum is :"+str(sum))
+
+# # # num1 = 10
+# # # num2 = 5
+# # # diff = num1 - num2
+# # # product = num1 * num2
+# # # print("the difference is :"+str(diff))
+# # # print("the product is :"+str(product))  
+
+# # name = input("what is your name? ")
+# # print("nice to meet you " + name)
+
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
+# total = num1 + num2
+# print("The total is: " + str(total))
+
