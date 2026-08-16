2501bbe34661e166250edd8015f53bf425164603
Hamzasameja <hamzayousufsameja@gmail.com>
2026-08-16 07:19:34 -0700

    return vs print in a same function

---
+++ python-practice/ReturnVSprint.py
@@ -0,0 +1,13 @@
+def multiply_print(a, b):
+    print(a * b)
+
+def multiply_return(a, b):
+    return a * b    
+
+multiply_print(4, 5)
+
+x = multiply_return(4, 5)
+print(x)
+
+y = multiply_print(4, 5)
+print(y)
