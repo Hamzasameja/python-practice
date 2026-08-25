2b252348472896a6be6caa73c652416843b97754
Hamzasameja <hamzayousufsameja@gmail.com>
2026-08-25 00:16:26 -0700

    combining functions,lists and dictionaries

---
+++ python-practice/combo-fun-lists-dic.py
@@ -0,0 +1,8 @@
+people = [
+    {"name": "Hamza", "age": 21},
+    {"name": "Ali", "age": 22},
+    {"name": "Yasmin", "age": 20}
+]
+
+for person in people:
+    print(person["name"] + " is " + str(person["age"]) + " years old.")
