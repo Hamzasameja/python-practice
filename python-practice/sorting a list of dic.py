9240d27b75d5871e33109b24c94e1ac2f9253952
Hamzasameja <hamzayousufsameja@gmail.com>
2026-08-28 10:56:33 -0700

    use of key lambda in sorting the specific lists in a dictionary

---
+++ python-practice/sorting a list of dic.py
@@ -0,0 +1,10 @@
+people = [
+    {"name": "Alice", "age": 30},
+    {"name": "Bob", "age": 25},
+    {"name": "Charlie", "age": 35}
+]
+
+people.sort(key=lambda person: person["age"])
+
+for person in people:
+    print(person["name"] + " - " + str(person["age"]))
