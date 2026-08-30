a7c99e05fa72ce465605c75118297ce5c75d9c19
Hamzasameja <hamzayousufsameja@gmail.com>
2026-08-30 11:55:12 -0700

    Handling multiple different errors

---
+++ python-practice/multiple-different-error.py
@@ -0,0 +1,9 @@
+try:
+    num1 = int(input("Enter a number: "))
+    num2 = int(input("Enter another number: "))
+    result = num1 / num2
+    print("The result is:" + str(result))
+except ValueError:
+    print("Invalid input. Please enter valid numbers.")
+except ZeroDivisionError:
+    print("You cannot divide by zero. Please enter a valid number.")
