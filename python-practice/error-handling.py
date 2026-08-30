f5a6c6e2edb7ceb0018dcc860f9fdb557e64403b
Hamzasameja <hamzayousufsameja@gmail.com>
2026-08-30 11:44:55 -0700

    use of try and except in error handling

---
+++ python-practice/error-handling.py
@@ -0,0 +1,6 @@
+try:
+    num = int(input("Enter a number: "))
+    print("You entered:" + str(num))
+
+except:
+    print("Invalid input. Please enter a valid number.")
---
+++ python-practice/writing to a file.py
@@ -0,0 +1,3 @@
+file = open("notes.txt", "w")
+file.write("Hello, this is my first file!")
+file.close()
