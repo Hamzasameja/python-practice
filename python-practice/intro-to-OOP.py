11065dfc796e20fba146061f1155ae5efe0feef7
Hamzasameja <hamzayousufsameja@gmail.com>
2026-09-05 12:25:27 -0700

    Classes and objects (intro in OOP)

---
+++ python-practice/intro-to-OOP.py
@@ -0,0 +1,11 @@
+# OBJECT -ORIENTED PROGRAMMING (OOP) IN PYTHON
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def greet(self):
+        print("Hello, my name is " + self.name + " and I am " + str(self.age) + " years old.")
+
+person1 = Person("Hamza", 30)
+person1.greet()
