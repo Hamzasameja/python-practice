46013627ffc4b4a5b0a529d9ff76cadc30218c03
Hamzasameja <hamzayousufsameja@gmail.com>
2026-09-05 12:57:43 -0700

    Multiple objects , same Class in OOP

---
+++ python-practice/mutilple-objects-same-class.py
@@ -0,0 +1,19 @@
+class person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def greet(self):
+        print("Hello, my name is " + self.name + " and I am " + str(self.age) + " years old.")
+
+    def birthday(self):
+        self.age += 1
+        print("Happy Birthday! You are now " + str(self.age) + " years old.")
+
+person1 = person("Hamza", 30)
+person2 = person("Ali", 30)
+
+person1.birthday()
+
+person1.greet()
+person2.greet()
