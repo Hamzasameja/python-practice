c7cf392a454617623043e8d10dd927f3dda5ddad
Hamzasameja <hamzayousufsameja@gmail.com>
2026-09-07 14:07:27 -0700

    using python's build in modules

---
+++ python-practice/random-modules.py
@@ -0,0 +1,8 @@
+import random
+
+numbers = random.randint(1, 10)
+print("Random number between 1 and 10:", numbers)
+
+fruits = ["apple", "banana", "cherry", "date", "elderberry"]
+random_fruit = random.choice(fruits)
+print("Random fruit from the list:", random_fruit)
