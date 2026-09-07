5736cd89fab9d22f0d679d0db2b0acdeb9745929
Hamzasameja <hamzayousufsameja@gmail.com>
2026-09-07 14:00:02 -0700

    importing only specific things

---
+++ python-practice/source.py
@@ -0,0 +1,5 @@
+def greet (name):
+    print("Hello, " + name + "! Welcome to the program.")
+
+def add(a, b):
+    return a + b
---
+++ python-practice/tree.py
@@ -0,0 +1,5 @@
+from source import greet, add
+
+greet("Hamza")
+result = add(5, 3)
+print("The sum is:", result)
